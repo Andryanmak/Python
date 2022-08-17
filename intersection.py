0fb6c17621ab4b24bb2fecfe0e26a131ccffc473
Mohd Ayan Khan <76160546+Ayan-Khan-47@users.noreply.github.com>
2022-08-17 17:22:26 +0530

    Create intersection.py

---
+++ intersection.py
@@ -0,0 +1,20 @@
+def intersection(A,B):
+    (C,m,n)=([],len(A),len(B))
+    (i,j)=(0,0)
+    while i+j<m+n:
+        if i==m:
+            break;
+        elif j==n:
+            break;
+        elif A[i]<B[j]:
+            i=i+1
+        elif A[i]>B[j]:
+            j=j+1
+        elif A[i]==B[j]:
+            C.append(A[i])
+            i=i+1 
+            j=j+1
+    return(C)
+a=[1,2,2,2,7,2,2,2,5,8]
+b=[1,2,6,7]
+print(setDifference(a,b))
