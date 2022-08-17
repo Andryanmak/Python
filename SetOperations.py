6ebbb9de972e877c3e8df38202f600a7b80b80d2
Mohd Ayan Khan <76160546+Ayan-Khan-47@users.noreply.github.com>
2022-08-17 17:54:35 +0530

    Update SetOperations.py

--- SetOperations.py
+++ SetOperations.py
@@ -25,6 +25,16 @@ def setDifference(A,B):
     return(C)
 
 
+def union(A,B):
+    (m,n)=(len(A),len(B))
+    (i,j)=(0,0)
+    A=[*set(A)]
+    for i in B:
+        if i not in A:
+            A=A+[i]
+    return(A)
+    
+
 a=[1,2,7,5,8]
 b=[1,2,6,7]
 print(intersection(a,b))
